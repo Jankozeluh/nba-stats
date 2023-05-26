a1b6e5f73408d4468302c17c6e7d5c42935a6ffe
Jan Koželuh <46165619+Jankozeluh@users.noreply.github.com>
2023-05-26 12:12:12 +0200

    - player_stats.py updated

--- src/player/player_metrics.py
+++ src/player/player_metrics.py
@@ -1,8 +1,8 @@
-import requests
 import streamlit as st
 import pandas as pd
 import plotly.express as px
-from nba_api.stats.endpoints import TeamDashLineups, CommonTeamRoster, TeamDashboardByGeneralSplits
+import streamlit as st
+from nba_api.stats.endpoints import CommonTeamRoster, TeamDashboardByGeneralSplits
 
 
 def calculate_efficiency_rating(player_career_data):
@@ -28,17 +28,35 @@ def calculate_scoring_efficiency(player_career_data):
     return player_career_data
 
 
-def calculate_triple_double_probability(player_career_data, player_averages):
-    # Implement your calculation logic here
-    # Use player_career_data and player_averages to estimate the probability
-    # of achieving a triple-double in a game
-    pass
+def calculate_triple_double_probability(player_career_data):
+    points = player_career_data['PTS'].mean()
+    rebounds = player_career_data['REB'].mean()
+    assists = player_career_data['AST'].mean()
+
+    triple_double_prob = ((points >= 10) + (rebounds >= 10) + (assists >= 10)) / 3
+
+    return triple_double_prob
 
 
-def calculate_win_shares(player_career_data):
-    # Implement your calculation logic here
-    # Use player_career_data to estimate the number of wins a player contributes
-    pass
+def calculate_impact_score(player_career_data):
+    # Compute an "impact score" as a simplistic alternative to win shares
+    # We'll use PTS, REB, AST, STL, BLK, and TOV
+    # The weights are arbitrary and you might want to adjust them
+    weights = {
+        "PTS": player_career_data["PTS"],
+        "REB": player_career_data["REB"],
+        "AST": player_career_data["AST"],
+        "STL": player_career_data["STL"],
+        "BLK": player_career_data["BLK"],
+        "TOV": player_career_data["TOV"]
+    }
+    impact_score = sum(player_career_data[stat].mean() * weight for stat, weight in weights.items())
+    return impact_score
+
+
+def calculate_clutch_shooting_percentage(player_career_data):
+    clutch_shooting_percentage = player_career_data['FG3_PCT'].mean()
+    return clutch_shooting_percentage
 
 
 def calculate_pie(player_career_data):
@@ -71,27 +89,66 @@ def calculate_pie(player_career_data):
     # Calculate PIE
     epsilon = 1e-8  # Small constant to avoid division by zero
     pie = (
-        (player_career_data["PTS"]
-         + player_career_data["REB"]
-         + player_career_data["AST"]
-         + player_career_data["STL"]
-         + player_career_data["BLK"]
-         - player_career_data["TOV"])
-        / (team_totals["PTS"]
-           + team_totals["REB"]
-           + team_totals["AST"]
-           + team_totals["STL"]
-           + team_totals["BLK"]
-           - team_totals["TOV"]
-           + epsilon)  # Add epsilon to avoid division by zero
+            (player_career_data["PTS"]
+             + player_career_data["REB"]
+             + player_career_data["AST"]
+             + player_career_data["STL"]
+             + player_career_data["BLK"]
+             - player_career_data["TOV"])
+            / (team_totals["PTS"]
+               + team_totals["REB"]
+               + team_totals["AST"]
+               + team_totals["STL"]
+               + team_totals["BLK"]
+               - team_totals["TOV"]
+               + epsilon)  # Add epsilon to avoid division by zero
     )
     return pie
 
 
-def calculate_clutch_shooting_percentage(player_career_data):
-    # Implement your calculation logic here
-    # Calculate the shooting percentage in clutch situations compared to overall shooting percentage
-    pass
+def calculate_scoring_composition(player_career_data):
+    # Check for missing values
+    if player_career_data[['FGM', 'FG3M', 'FTM', 'PTS']].isnull().any().any():
+        st.warning('Missing data for scoring composition. Cannot plot.')
+        return None, None, None
+
+    # Calculate total points from 2PT field goals, 3PT field goals, and free throws
+    pts_from_2pt_fg = player_career_data['FGM'] * 2 - player_career_data['FG3M'] * 3
+    pts_from_3pt_fg = player_career_data['FG3M'] * 3
+    pts_from_ft = player_career_data['FTM'] * 1
+
+    # Calculate scoring composition percentages
+    total_pts = player_career_data['PTS']
+    pts_from_2pt_fg_pct = pts_from_2pt_fg / total_pts
+    pts_from_3pt_fg_pct = pts_from_3pt_fg / total_pts
+    pts_from_ft_pct = pts_from_ft / total_pts
+
+    return pts_from_2pt_fg_pct, pts_from_3pt_fg_pct, pts_from_ft_pct
+
+
+def visualize_scoring_composition(pts_from_2pt_fg_pct, pts_from_3pt_fg_pct, pts_from_ft_pct):
+    # Ensure the percentages are float
+    pts_from_2pt_fg_pct = pts_from_2pt_fg_pct.values[0]
+    pts_from_3pt_fg_pct = pts_from_3pt_fg_pct.values[0]
+    pts_from_ft_pct = pts_from_ft_pct.values[0]
+
+    # Create DataFrame for plotting
+    scoring_composition = pd.DataFrame({
+        'Scoring Source': ['2PT Field Goals', '3PT Field Goals', 'Free Throws'],
+        'Percentage': [pts_from_2pt_fg_pct, pts_from_3pt_fg_pct, pts_from_ft_pct]
+    })
+
+    # Plot bar chart
+    fig = px.bar(scoring_composition, x='Scoring Source', y='Percentage',
+                 title='Scoring Composition', color='Scoring Source',
+                 color_discrete_sequence=px.colors.qualitative.Pastel)
+
+    fig.update_layout(xaxis_title='Scoring Source',
+                      yaxis_title='Percentage of Total Points',
+                      yaxis=dict(
+                          tickformat=".2%",
+                      ))
+    st.plotly_chart(fig)
 
 
 def visualize_efficiency_rating(efficiency_rating):
@@ -113,18 +170,17 @@ def visualize_pie(pie):
     st.plotly_chart(fig)
 
 
-def display_player_metrics(player_career_data, selected_seasons):
+def display_player_metrics(player_career_data):
     st.subheader("Player Metrics")
 
-    # Calculate efficiency rating
     efficiency_rating = calculate_efficiency_rating(player_career_data)
     visualize_efficiency_rating(efficiency_rating)
 
     scoring_efficiency_data = calculate_scoring_efficiency(player_career_data)
     visualize_scoring_efficiency(scoring_efficiency_data)
 
-    # Calculate and visualize other metrics
-
-    # Example: Calculate and visualize PIE
     pie = calculate_pie(player_career_data)
     visualize_pie(pie)
+
+    pts_from_2pt_fg_pct, pts_from_3pt_fg_pct, pts_from_ft_pct = calculate_scoring_composition(player_career_data)
+    visualize_scoring_composition(pts_from_2pt_fg_pct, pts_from_3pt_fg_pct, pts_from_ft_pct)
--- src/player/player_stats.py
+++ src/player/player_stats.py
@@ -8,6 +8,7 @@ from src.player.nba_api_utils import (
     get_player_career_stats,
 )
 from src.player.player_metrics import *
+import numpy as np
 
 
 def show_player_stats():
@@ -22,62 +23,105 @@ def show_player_stats():
             player_info = get_player_info(player_id)
             player_career_stats = get_player_career_stats(player_id)
 
-            # Display player information
             st.subheader("Player Information")
-            st.subheader(player_info["Name"][0])
+
+            player_photo_url = f"https://cdn.nba.com/headshots/nba/latest/260x190/{player_id}.png"
+            response = requests.get(player_photo_url, stream=True)
+
             col1, col2 = st.columns(2)
+
             with col1:
+                if response.status_code == 200:
+                    st.image(player_photo_url, use_column_width=True, output_format="PNG", width=200)
+                else:
+                    st.image('https://cdn.nba.com/headshots/nba/latest/260x190/fallback.png', use_column_width=True,
+                             output_format="PNG")
+
+            with col2:
+                st.subheader(player_info["Name"][0])
                 st.write("**Team:**", player_info["Team"][0])
                 st.write("**College:**", player_info["College"][0])
                 st.write("**Position:**", player_info["Position"][0])
-            with col2:
                 st.write("**Height:**", player_info["Height"][0])
                 st.write("**Weight:**", player_info["Weight"][0])
 
             st.divider()
 
+            triple_double_prob = calculate_triple_double_probability(player_career_stats)
+            impact_score = calculate_impact_score(player_career_stats)
+            clutch_shooting_percentage = calculate_clutch_shooting_percentage(player_career_stats)
+
+            # Display stats
+            col1, col2, col3 = st.columns(3)
+            with col1:
+                st.markdown(f"**Triple-Double Probability:** {triple_double_prob:.2%}")
+            with col2:
+                st.markdown(f"**Average Impact Score:** \n{impact_score.mean():.2f}")
+            with col3:
+                st.markdown(f"**Clutch Shooting Percentage:** {clutch_shooting_percentage:.2%}")
+
+        st.divider()
+
+        with st.container():  # Use this for a separate box
+            display_player_metrics(player_career_stats)
+            st.divider()
+
+        with st.container():
             st.subheader("Stats")
             selected_seasons = st.multiselect("Select seasons", player_career_stats["SEASON_ID"].unique())
-
-            display_player_metrics(player_career_stats, selected_seasons)
-            st.divider()
             display_player_stats(player_career_stats, selected_seasons)
 
-            player_photo_url = f"https://cdn.nba.com/headshots/nba/latest/260x190/{player_id}.png"
-            response = requests.get(player_photo_url, stream=True)
-            if response.status_code == 200:
-                st.image(player_photo_url, use_column_width=True)
-            else:
-                st.image('https://cdn.nba.com/headshots/nba/latest/260x190/fallback.png')
 
-
+# def display_player_stats(player_career_data, selected_seasons):
+#     for season in selected_seasons:
+#         st.subheader(f"Season {season}")
+#         season_data = player_career_data[player_career_data["SEASON_ID"] == season]
+#
+#         team = season_data["TEAM_ABBREVIATION"].values[0]
+#         st.write(f"**Team:** {team}")
+#         #     # Bar Chart for Points
+#         #     display_bar_chart(season_data, x="PLAYER_ID", y="PTS"))
+#         #     # Line Chart for Assists
+#         #     display_line_chart(season_data, x="PLAYER_ID", y="AST")
+#         #     # Scatter Chart for Field Goal Percentage and Free Throw Percentage
+#         #     display_scatter_chart(season_data, x="FG_PCT", y="FT_PCT")
+#
+#         # Display statistics as numbers in little boxes
+#         stats_columns = ["PTS", "AST", "REB", "FG_PCT", "FT_PCT", "STL", "BLK"]
+#         num_stats = len(stats_columns)
+#         num_columns = min(num_stats, 2)
+#         num_rows = -(-num_stats // num_columns)  # Ceiling division
+#
+#         col1, col2 = st.columns(num_columns)
+#
+#         for i, column in enumerate(stats_columns):
+#             stat_value = season_data[column].values[0]
+#             if i < num_rows:
+#                 with col1:
+#                     st.metric(label=column, value=stat_value)
+#             else:
+#                 with col2:
+#                     st.metric(label=column, value=stat_value)
 def display_player_stats(player_career_data, selected_seasons):
     for season in selected_seasons:
         st.subheader(f"Season {season}")
         season_data = player_career_data[player_career_data["SEASON_ID"] == season]
-
+        st.write(f"**Age:** {season_data['PLAYER_AGE'].values[0].astype(int)}")
         team = season_data["TEAM_ABBREVIATION"].values[0]
         st.write(f"**Team:** {team}")
-        #     # Bar Chart for Points
-        #     display_bar_chart(season_data, x="PLAYER_ID", y="PTS"))
-        #     # Line Chart for Assists
-        #     display_line_chart(season_data, x="PLAYER_ID", y="AST")
-        #     # Scatter Chart for Field Goal Percentage and Free Throw Percentage
-        #     display_scatter_chart(season_data, x="FG_PCT", y="FT_PCT")
+        season_data = season_data.drop(season_data.columns[[0, 1, 2, 3, 4, 5]], axis=1)
 
-        # Display statistics as numbers in little boxes
-        stats_columns = ["PTS", "AST", "REB", "FG_PCT", "FT_PCT", "STL", "BLK"]
+        # Define a list of statistics columns to display, this should include all numeric columns
+        stats_columns = [col for col in season_data.columns if np.issubdtype(season_data[col].dtype, np.number)]
+
+        # Calculate the number of columns for display (limiting it to a maximum of 4 for layout purposes)
         num_stats = len(stats_columns)
-        num_columns = min(num_stats, 2)
-        num_rows = -(-num_stats // num_columns)  # Ceiling division
+        num_columns = min(num_stats, 4)
+        columns = st.columns(num_columns)
 
-        col1, col2 = st.columns(num_columns)
-
+        # Iterate through each statistic
         for i, column in enumerate(stats_columns):
-            stat_value = season_data[column].values[0]
-            if i < num_rows:
-                with col1:
-                    st.metric(label=column, value=stat_value)
-            else:
-                with col2:
-                    st.metric(label=column, value=stat_value)
+            # Calculate which column to place this metric in
+            col_idx = i % num_columns
+            stat_value = season_data[column].mean()
+            columns[col_idx].metric(label=column, value=f"{stat_value:.2f}")
